935819ad9af218651fc2fe6c48217d4cfbe46ba0
razaqnejad <razaqnejad@gmail.com>
2024-04-23 11:12:03 +0330

    cleanup code

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,27 +1,21 @@
 import pandas as pd
 import os
 
-# بارگذاری داده‌های CSV
-data = pd.read_csv('data/dataset.csv', skiprows=1)  # اگر سربرگ ندارید
+# load CSV file
+data = pd.read_csv('data/dataset.csv', skiprows=1)
 
-# تعیین مسیر پوشه ریشه برای ذخیره فایل‌های CSV
+# create destination directory
 root_save_path = 'data/train/'
-
-# اطمینان از ایجاد پوشه ریشه اگر پیش‌تر ایجاد نشده است
 if not os.path.exists(root_save_path):
     os.makedirs(root_save_path)
 
-# گروه‌بندی داده‌ها بر اساس برچسب
+# group images by lable
 grouped = data.groupby(data.columns[0])
 
-# حلقه برای ذخیره هر گروه در فایل CSV جداگانه
+# extract and save data by in defferent directories
 for label, group in grouped:
-    # تعیین مسیر پوشه برای ذخیره فایل‌های CSV بر اساس برچسب
     label_save_path = os.path.join(root_save_path, str(label))
-    # اطمینان از ایجاد پوشه برچسب اگر پیش‌تر ایجاد نشده است
     if not os.path.exists(label_save_path):
         os.makedirs(label_save_path)
-    
-    # تعیین مسیر فایل CSV بر اساس برچسب و ذخیره آن
     csv_file_path = os.path.join(label_save_path, f'label_{label}.csv')
     group.to_csv(csv_file_path, header=False, index=False)
--- scripts/extract_images.py
+++ scripts/extract_images.py
@@ -10,27 +10,23 @@ def load_data(filepath):
     """Load CSV data into a DataFrame."""
     return pd.read_csv(filepath)
 
-# بارگذاری داده‌های CSV
+# load CSV data
 data = pd.read_csv('data/dataset.csv', skiprows=1)
 
-# تعیین مسیر پوشه برای ذخیره تصاویر
 save_path = 'data/image_files/'
-
-# اطمینان از ایجاد پوشه اگر پیش‌تر ایجاد نشده است
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-# حلقه برای تبدیل رشته پیکسل‌ها به تصویر و ذخیره آن‌ها
+# read dataset.csv data {lable, pixcels}
 for index, row in data.iterrows():
-    # استخراج برچسب و پیکسل‌ها
     label = row.iloc[0]
-    pixels = row.iloc[1].split(' ')  # فرض بر این است که پیکسل‌ها با فاصله جدا شده‌اند
-    pixels = list(map(int, pixels))  # تبدیل پیکسل‌ها به اعداد صحیح
+    pixels = row.iloc[1].split(' ')
+    pixels = list(map(int, pixels))
     
-    # محاسبه ابعاد تصویر
-    image_size = int(math.sqrt(len(pixels)))  # فرض بر این است که تصویر مربعی است
+    # calculate image real sizes
+    image_size = int(math.sqrt(len(pixels)))
     image_array = np.array(pixels, dtype=np.uint8).reshape((image_size, image_size))
     
-    # ایجاد تصویر و ذخیره آن
+    # creat image and save
     image = Image.fromarray(image_array)
     image.save(os.path.join(save_path, f'label_{label}_index_{index}.png'))
